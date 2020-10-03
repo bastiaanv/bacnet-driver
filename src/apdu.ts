2bbabb342121ed5f034b64faf5b933f9991be2ed
Bastiaan Verhaar <bastiankpn7800@gmail.com>
2020-10-03 17:50:39 +0200

    Fix application tags

--- src/apdu.ts
+++ src/apdu.ts
@@ -57,7 +57,7 @@ export class ApplicationProtocolDataUnit {
 
     public static decodeTag(buffer: TransporterBuffer): {tagNumber: number, length: number} {
         // Check if tag is an extended value
-        if ((buffer.buffer[buffer.offset] & 0x05) > 0) {
+        if ((buffer.buffer[buffer.offset] & 0x05) === 5) {
             const part1 = buffer.buffer[buffer.offset++];
             const length = buffer.buffer[buffer.offset++];
 
