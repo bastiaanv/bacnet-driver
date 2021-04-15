fd1dbad92e0c0accd4eac6de6ab82b081a07dd1d
bastiaanv <bastiankpn7800@gmail.com>
2021-04-15 21:24:51 +0200

    Fix exports

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,6 +2,7 @@ export * from './events/writeProperty/write.property.options';
 export * from './events/readProperty/read.property.options';
 export * from './events/error/error.message';
 export * from './events/i.am';
+export * from './events/notification/notification.event';
 export * from './events/readProperty/read.number';
 export * from './events/readProperty/read.string';
 export * from './events/readProperty/read.object';
